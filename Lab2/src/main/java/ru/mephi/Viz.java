c4e4246b2b9f967e927634e9da013b10e440a0ba
Антон Михалев <anton150801@gmail.com>
2021-11-26 10:41:45 +0300

    name edges

--- Lab2/src/main/java/ru/mephi/Viz.java
+++ Lab2/src/main/java/ru/mephi/Viz.java
@@ -2,16 +2,9 @@ package ru.mephi;
 
 import com.mxgraph.layout.*;
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
-import com.mxgraph.layout.orthogonal.mxOrthogonalLayout;
-import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxCellRenderer;
-import com.mxgraph.view.mxGraph;
-import javafx.scene.shape.Circle;
 import javafx.util.Pair;
-import org.jgraph.graph.CellHandle;
-import org.jgraph.graph.CellView;
 import org.jgrapht.ext.JGraphXAdapter;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.DirectedWeightedPseudograph;
 
@@ -26,19 +19,20 @@ import java.util.*;
 
 class MyEdge extends DefaultWeightedEdge {
     public String edgeName;
-    public static int curEdge = 0;
+    public boolean startNode = false;
 
-    public MyEdge(String edgeName) {
-        this.edgeName = edgeName;
+    @Override
+    public String toString() {
+        return edgeName;
     }
 
 }
 public class Viz {
-    public static DirectedWeightedPseudograph<String, String> createGraph(SoftReference<NFA> nfa) throws IOException {
+    public static DirectedWeightedPseudograph<String, MyEdge> createGraph(SoftReference<NFA> nfa) throws IOException {
         File imgFile = new File("graph.png");
         imgFile.createNewFile();
-        DirectedWeightedPseudograph<String, String> g =
-                new DirectedWeightedPseudograph<String, String>(String.class);
+        DirectedWeightedPseudograph<String, MyEdge> g =
+                new DirectedWeightedPseudograph<String, MyEdge>(MyEdge.class);
         for (NFANode nfaNode : nfa.get().nodes) {
             String curVertex = String.valueOf(nfaNode.getId());
             g.addVertex(curVertex);
@@ -54,21 +48,18 @@ public class Viz {
                 }
                 if (Objects.equals(curEdge, "CIRCUMFLEXUS"))
                     curEdge = "^";
-                MyEdge tmpEdge = new MyEdge(curEdge);
-                if (g.containsEdge(tmpEdge.edgeName)){
-                    tmpEdge.edgeName += MyEdge.curEdge;
-                    ++MyEdge.curEdge;
-                }
+                MyEdge tmpEdge = new MyEdge();
+                tmpEdge.edgeName = curEdge;
                 String destVertex = String.valueOf(elem.getKey().get().getId());
-                g.addEdge(sourceVertex, destVertex, tmpEdge.edgeName);
+                g.addEdge(sourceVertex, destVertex, tmpEdge);
             }
         }
         return g;
     }
 
     public static void givenAdaptedGraph_whenWriteBufferedImage_thenFileShouldExist(SoftReference<NFA> nfa) throws IOException {
-        JGraphXAdapter<String, String> graphAdapter =
-                new JGraphXAdapter<String, String>(createGraph(nfa));
+        JGraphXAdapter<String, MyEdge> graphAdapter =
+                new JGraphXAdapter<String, MyEdge>(createGraph(nfa));
         mxIGraphLayout layout = new mxHierarchicalLayout(graphAdapter); // mxHierarchicalLayout
         layout.execute(graphAdapter.getDefaultParent());
         BufferedImage image =
@@ -78,7 +69,7 @@ public class Viz {
     }
 
     public static void main(String[] args) throws IOException {
-        String str = "a|(3:n)";
+        String str = "a|^";
         AbstractSyntaxTree tree = new AbstractSyntaxTree(str);
         Node rootNode = tree.buildTree();
         tree.doOrder(rootNode);
@@ -86,12 +77,6 @@ public class Viz {
         SoftReference<NFA> nfaSR = new SoftReference<>(nfa);
         System.out.println("StartNode: " + nfa.getStart().get().getId());
         System.out.println("EndNode: " + nfa.getEnd().get().getId());
-        int i = 0;
-        String[] edgesName = new String[nfaSR.get().alphabet.size()];
-        for(String c : nfaSR.get().alphabet){
-            edgesName[i] = c;
-            ++i;
-        }
         givenAdaptedGraph_whenWriteBufferedImage_thenFileShouldExist(nfaSR);
     }
 }
